94755dc83552f8b9edce24ccef45e9237f9596e5
DeedleFake <deedlefake@users.noreply.github.com>
2023-08-25 00:37:34 -0400

    xiter: add `Pull()` and use it to massively clean up, but slow down, `Zip()`

--- transform.go
+++ transform.go
@@ -55,77 +55,20 @@ type Zipped[T1, T2 any] struct {
 	OK2 bool
 }
 
-func send[T any](done <-chan struct{}, c chan<- T) func(v T) bool {
-	return func(v T) bool {
-		select {
-		case <-done:
-			return false
-		case c <- v:
-			return true
-		}
-	}
-}
-
 // Zip returns a new Seq that yields the values of seq1 and seq2
 // simultaneously.
-//
-// Important thing to note: Because of the lack of corountine support,
-// seq1 and seq2 are called in new threads. This means not only is
-// there some performance overhead for simple cases, but also that
-// they should not do anything that isn't thread-safe without some
-// other form of synchronization.
 func Zip[T1, T2 any](seq1 Seq[T1], seq2 Seq[T2]) Seq[Zipped[T1, T2]] {
-	done := make(chan struct{})
-
-	c1 := make(chan T1)
-	go func() {
-		defer close(c1)
-		seq1(send(done, c1))
-	}()
-
-	c2 := make(chan T2)
-	go func() {
-		defer close(c2)
-		seq2(send(done, c2))
-	}()
-
 	return func(yield func(Zipped[T1, T2]) bool) bool {
-		defer close(done)
-
-		var c1hold chan T1
-		var c2hold chan T2
-		var val Zipped[T1, T2]
+		p1, stop := Pull(seq1)
+		defer stop()
+		p2, stop := Pull(seq2)
+		defer stop()
 
 		for {
-			select {
-			case v, ok := <-c1:
-				if !ok {
-					c1 = nil
-					goto send
-				}
-				c1, c1hold = nil, c1
-				val.V1 = v
-				val.OK1 = true
-			case v, ok := <-c2:
-				if !ok {
-					c2 = nil
-					goto send
-				}
-				c2, c2hold = nil, c2
-				val.V2 = v
-				val.OK2 = true
-			}
-
-		send: // Dear lord, what is even going on here?
-			if (val.OK1 || (c1 == c1hold)) && (val.OK2 || (c2 == c2hold)) && !(c1==c1hold && c2==c2hold) {
-				if !yield(val) {
-					return false
-				}
-				c1, c1hold, c2, c2hold = c1hold, nil, c2hold, nil
-				val = Zipped[T1, T2]{}
-			}
-
-			if (c1 == c1hold) && (c2 == c2hold) {
+			var val Zipped[T1, T2]
+			val.V1, val.OK1 = p1()
+			val.V2, val.OK2 = p2()
+			if (!val.OK1 && !val.OK2) || !yield(val) {
 				return false
 			}
 		}
--- xiter.go
+++ xiter.go
@@ -2,6 +2,8 @@
 // with, but not requiring, CL 510541.
 package xiter
 
+import "sync"
+
 // Seq represents an iterator over a sequence of values. When called,
 // the passed yield function is called for each successive value.
 // Returning false from yield causes the iterator to stop, equivalent
@@ -9,3 +11,44 @@ package xiter
 // is completely ignored, but present to be compatible with the CL
 // 510541 prototype.
 type Seq[T any] func(yield func(T) bool) bool
+
+// Pull simulates a pull-iterator using Go's built-in concurrency
+// primitives in lieu of coroutines. It handles all synchronization
+// internally, so despite running the iterator in a new thread, there
+// shouldn't be any data races, but there is some performance
+// overhead.
+//
+// The returned stop function must be called when the iterator is no
+// longer in use.
+func Pull[T any](seq Seq[T]) (iter func() (T, bool), stop func()) {
+	next := make(chan struct{})
+	yield := make(chan T)
+
+	go func() {
+		defer close(yield)
+
+		_, ok := <-next
+		if !ok {
+			return
+		}
+
+		seq(func(v T) bool {
+			yield <- v
+			_, ok := <-next
+			return ok
+		})
+	}()
+
+	return func() (v T, ok bool) {
+			select {
+			case <-yield:
+				return v, false
+			case next <- struct{}{}:
+				v, ok := <-yield
+				return v, ok
+			}
+		}, sync.OnceFunc(func() {
+			close(next)
+			<-yield
+		})
+}
