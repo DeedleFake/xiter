3db7d28747f607a17df231341cf511f34e6eefd8
DeedleFake <deedlefake@users.noreply.github.com>
2024-01-16 13:47:21 -0500

    internal/gen: a bunch more implementation of code generation

--- internal/gen/gen.go
+++ internal/gen/gen.go
@@ -3,8 +3,10 @@ package main
 import (
 	"bytes"
 	_ "embed"
+	"fmt"
 	"go/format"
 	"go/types"
+	"log/slog"
 	"os"
 	"regexp"
 	"strings"
@@ -19,11 +21,69 @@ var (
 	//go:embed output.go.tmpl
 	outputTemplate string
 	tmpl           = template.Must(template.New("output").Funcs(funcMap).Parse(outputTemplate))
-	funcMap        = map[string]any{"convertName": convertName}
+	funcMap        = map[string]any{
+		"convertName":    convertName,
+		"typeParamSlice": listToSlice[*types.TypeParam],
+		"tupleSlice":     listToSlice[*types.Var],
+		"convertType":    convertType,
+	}
 )
 
 func convertName(name string) string {
 	return strings.TrimPrefix(name, "_")
+}
+
+type List[T any] interface {
+	At(int) T
+	Len() int
+}
+
+func listToSlice[T any](list List[T]) []T {
+	length := list.Len()
+	s := make([]T, 0, length)
+	for i := 0; i < length; i++ {
+		s = append(s, list.At(i))
+	}
+	return s
+}
+
+func convertType(rangefunc bool, t types.Type) string {
+	switch t := t.(type) {
+	case *types.Named:
+		var pkg string
+		if t.Obj().Pkg() != nil && t.Obj().Pkg().Path() != "deedles.dev/xiter" {
+			pkg = t.Obj().Pkg().Name() + "."
+		}
+
+		if t.TypeArgs().Len() == 0 {
+			return pkg + t.Obj().Name()
+		}
+
+		typeArgs := make([]string, 0, t.TypeArgs().Len())
+		for _, arg := range listToSlice(t.TypeArgs()) {
+			typeArgs = append(typeArgs, convertType(rangefunc, arg))
+		}
+		return fmt.Sprintf("%v%v[%v]", pkg, t.Obj().Name(), strings.Join(typeArgs, ","))
+
+	case *types.Slice:
+		return fmt.Sprintf("[]%v", convertType(rangefunc, t.Elem()))
+
+	case *types.Interface, *types.Basic, *types.TypeParam, *types.Signature, *types.Chan:
+		return t.String()
+
+	default:
+		return fmt.Sprintf("\"%T\"", t)
+	}
+}
+
+func join[T any](sep string, s []T) string {
+	var i string
+	var buf strings.Builder
+	for _, v := range s {
+		fmt.Fprintf(&buf, "%v%v", i, v)
+		i = sep
+	}
+	return buf.String()
 }
 
 func load() []*types.Func {
@@ -58,9 +118,10 @@ func write(name string, funcs []*types.Func, rangefunc bool) {
 	}
 
 	b := buf.Bytes()
-	b, err = format.Source(b)
+	formatted, err := format.Source(b)
 	if err != nil {
-		panic(err)
+		slog.Error("format", "file", name, "err", err)
+		formatted = b
 	}
 
 	file, err := os.Create(name)
@@ -69,7 +130,7 @@ func write(name string, funcs []*types.Func, rangefunc bool) {
 	}
 	defer file.Close()
 
-	_, err = file.Write(b)
+	_, err = file.Write(formatted)
 	if err != nil {
 		panic(err)
 	}
