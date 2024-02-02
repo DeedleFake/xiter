35c14bfc26cfe96d6c9dddb81bf0bc9cff302c93
DeedleFake <deedlefake@users.noreply.github.com>
2024-02-02 17:40:41 -0500

    internal/xslices: remove

--- internal/xslices/xslices.go
+++
@@ -1,9 +0,0 @@
-package xslices
-
-func Map[From, To any](from []From, f func(From) To) []To {
-	to := make([]To, 0, len(from))
-	for _, v := range from {
-		to = append(to, f(v))
-	}
-	return to
-}
