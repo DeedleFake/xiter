b5a470b206a72338e5a75ee71aa0e478ab408b91
David Chase <drchase@google.com>
2024-02-15 10:15:31 -0500

    Update to make aware of GOEXPERIMENT=rangefunc iter package (#2)
    This will (attempt to) copy the types, and will use the
    coroutine version of "Pull" (which performs much better).
    
    The old goroutine version of Pull is renamed GoPull, and
    is used for Pull when the rangefunc GOEXPERIMENT is not set.

---
+++ xiter_norangefunc.go
@@ -0,0 +1,16 @@
+//go:build !goexperiment.rangefunc
+
+package xiter
+
+// Seq represents an iterator over a sequence of values. When called,
+// the passed yield function is called for each successive value.
+// Returning false from yield causes the iterator to stop, equivalent
+// to a break statement.
+type Seq[T any] func(yield func(T) bool)
+
+// Seq2 represents a two-value iterator.
+type Seq2[T1, T2 any] func(yield func(T1, T2) bool)
+
+func Pull[T any](seq Seq[T]) (iter func() (T, bool), stop func()) {
+	return GoPull[T](seq)
+}
