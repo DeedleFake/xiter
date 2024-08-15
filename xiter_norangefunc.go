9327f91bed7374ff0122c8a57840a605425b975f
DeedleFake <deedlefake@users.noreply.github.com>
2024-08-15 16:13:17 -0400

    xiter: add godoc comment to `Pull()`

--- xiter_norangefunc.go
+++ xiter_norangefunc.go
@@ -11,6 +11,10 @@ type Seq[T any] func(yield func(T) bool)
 // Seq2 represents a two-value iterator.
 type Seq2[T1, T2 any] func(yield func(T1, T2) bool)
 
+// Pull converts a push iterator in the form of a [Seq] into a pull
+// iterator. On versions of Go that have support for this capability
+// natively, that is used, while on versions of Go prior to that this
+// is simply a wrapper around [GoPull].
 func Pull[T any](seq Seq[T]) (iter func() (T, bool), stop func()) {
 	return GoPull[T](seq)
 }
--- xiter_rangefunc.go
+++ xiter_rangefunc.go
@@ -13,6 +13,10 @@ type Seq[T any] iter.Seq[T] // Type alias would be nice, but not supported for g
 // Seq2 represents a two-value iterator.
 type Seq2[T1, T2 any] iter.Seq2[T1, T2]
 
+// Pull converts a push iterator in the form of a [Seq] into a pull
+// iterator. On versions of Go that have support for this capability
+// natively, that is used, while on versions of Go prior to that this
+// is simply a wrapper around [GoPull].
 func Pull[T any](seq Seq[T]) (iterator func() (T, bool), stop func()) {
 	return iter.Pull[T](iter.Seq[T](seq))
 }
