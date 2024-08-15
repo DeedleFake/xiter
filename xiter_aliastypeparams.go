9a391e3020cf28e5fb381af0ec96c2fe600284c3
DeedleFake <deedlefake@users.noreply.github.com>
2024-08-15 16:10:16 -0400

    xiter: add support for `GOEXPERIMENT=aliastypeparams`

--- xiter_aliastypeparams.go
+++ xiter_aliastypeparams.go
@@ -1,4 +1,4 @@
-//go:build (goexperiment.rangefunc || go1.23) && !goexperiment.aliastypeparams
+//go:build goexperiment.aliastypeparams
 
 package xiter
 
@@ -8,10 +8,10 @@ import "iter"
 // the passed yield function is called for each successive value.
 // Returning false from yield causes the iterator to stop, equivalent
 // to a break statement.
-type Seq[T any] iter.Seq[T] // Type alias would be nice, but not supported for generic types.
+type Seq[T any] = iter.Seq[T]
 
 // Seq2 represents a two-value iterator.
-type Seq2[T1, T2 any] iter.Seq2[T1, T2]
+type Seq2[T1, T2 any] = iter.Seq2[T1, T2]
 
 func Pull[T any](seq Seq[T]) (iterator func() (T, bool), stop func()) {
 	return iter.Pull[T](iter.Seq[T](seq))
